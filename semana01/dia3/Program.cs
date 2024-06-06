fcf66a98ef27ac8d412554d42e1d2b41ca86cbec
César Mayta <cesarmayta@gmail.com>
2024-06-05 21:43:39 -0500

    dia3 monedas con funciones

--- semana01/dia3/Program.cs
+++ semana01/dia3/Program.cs
@@ -5,11 +5,9 @@ double montoOrigen = 0;
 double montoDestino = 0;
 int opcion = 0;
 
-// PROCESO
-while(opcion != 3)
+//FUNCIONES
+void MostrarMenu()
 {
-    // ENTRADAS
-    Console.Clear();
     Console.WriteLine("===================================");
     Console.WriteLine(" CONVERTIR DE MONEDAS VERSION 1.0");
     Console.WriteLine("===================================");
@@ -18,25 +16,60 @@ while(opcion != 3)
     OPCIÓN 2 : Convertir de dolares a soles
     OPCIÓN 3 : Salir
     ");
+}
+
+//void ConvertirSolesDolares()
+//{
+//    Console.WriteLine("ELEGISTE CONVERTIR DE SOLES A DOLARES");
+//    Console.WriteLine("Ingrese Monto en Soles :");
+//    montoOrigen = double.Parse(Console.ReadLine());
+//    montoDestino = montoOrigen / valorVenta;
+//    Console.WriteLine($"{montoOrigen} soles es igual a {Math.Round(montoDestino, 2)} dolares");
+//}
+
+//void ConvertirDolaresSoles()
+//{
+//    Console.WriteLine("ELEGISTE CONVERTIR DE DOLARES A SOLES");
+//    Console.WriteLine("Ingrese el Monto en Dolares : ");
+//    montoOrigen = double.Parse(Console.ReadLine());
+//    montoDestino = montoOrigen * valorCompra;
+//    Console.WriteLine($"{montoOrigen} dolares es igual a {Math.Round(montoDestino, 2)} soles");
+//}
+
+void ConvertirMoneda(string monedaOrigen,string monedaDestino)
+{
+    Console.WriteLine($"ELEGISTE CONVERTIR DE {monedaOrigen.ToUpper()} A {monedaDestino.ToUpper()}");
+    Console.WriteLine($"Ingrese Monto en {monedaOrigen.ToLower()} :");
+    montoOrigen = double.Parse(Console.ReadLine());
+    if (monedaOrigen == "soles" && monedaDestino == "dolares")
+    {
+        montoDestino = montoOrigen / valorVenta;
+    }
+    else if(monedaOrigen == "dolares" && monedaDestino == "soles")
+    {
+        montoDestino = montoOrigen * valorVenta;
+    }
+    
+    Console.WriteLine($"{montoOrigen} {monedaOrigen.ToLower()} es igual a {Math.Round(montoDestino, 2)} {monedaDestino.ToLower()}");
+}
+
+// PROCESO
+while(opcion != 3)
+{
+    // ENTRADAS
+    Console.Clear();
+    MostrarMenu();
     Console.WriteLine("Ingrese una opción del menú : ");
     opcion = int.Parse(Console.ReadLine());
     switch (opcion)
     {
         case 1:
             Console.Clear();
-            Console.WriteLine("ELEGISTE CONVERTIR DE SOLES A DOLARES");
-            Console.WriteLine("Ingrese Monto en Soles :");
-            montoOrigen = double.Parse(Console.ReadLine());
-            montoDestino = montoOrigen / valorVenta;
-            Console.WriteLine($"{montoOrigen} soles es igual a {Math.Round(montoDestino,2)} dolares");
+            ConvertirMoneda("soles","dolares");
             break;
         case 2:
             Console.Clear();
-            Console.WriteLine("ELEGISTE CONVERTIR DE DOLARES A SOLES");
-            Console.WriteLine("Ingrese el Monto en Dolares : ");
-            montoOrigen = double.Parse(Console.ReadLine());
-            montoDestino = montoOrigen * valorCompra;
-            Console.WriteLine($"{montoOrigen} dolares es igual a {Math.Round(montoDestino,2)} soles");
+            ConvertirMoneda("dolares", "soles");
             break;
         case 3:
             Console.Clear();
