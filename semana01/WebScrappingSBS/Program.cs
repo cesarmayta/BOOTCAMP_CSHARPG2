aafe64cffc5e6b61aece68e20b4223b0224e67b9
César Mayta <cesarmayta@gmail.com>
2024-06-05 22:16:33 -0500

    scrapping table

---
+++ semana01/WebScrappingSBS/Program.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://www.sbs.gob.pe/app/pp/sistip_portal/paginas/publicacion/tipocambiopromedio.aspx";
+
+        using HttpClient client = new HttpClient();
+
+        var response = await client.GetStringAsync(url);
+
+        if (response != null)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(response);
+
+            var table = html.DocumentNode.SelectSingleNode("//table[@id='ctl00_cphContent_rgTipoCambio_ctl00']");
+            Console.WriteLine(table.InnerHtml);
+
+            
+        }
+    }
+}
+
