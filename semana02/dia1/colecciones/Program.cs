e65560204d808630cad295f795233360c8d25d2e
César Mayta <cesarmayta@gmail.com>
2024-06-10 19:34:40 -0500

    semana 2 dia 1 colecciones

---
+++ semana02/dia1/colecciones/Program.cs
@@ -0,0 +1,16 @@
+﻿//int a = 0;
+//string nombre = "cesar";
+//double total = 0;
+//array
+int[] notas;
+int cantidad = 0;
+Console.WriteLine("INGRESE CANTIDAD DE NOTAS : ");
+cantidad = Convert.ToInt32(Console.ReadLine());
+
+notas = new int[cantidad];
+for (int i = 0; i < cantidad; i++)
+{
+    Console.WriteLine($"INGRESE NOTA {i + 1} : ");
+    notas[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("se ingresaron todas las notas...");
