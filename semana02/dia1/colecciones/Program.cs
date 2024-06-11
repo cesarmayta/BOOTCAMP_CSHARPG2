fa0cb54653465d3b8f08a6886a941217561fe09f
César Mayta <cesarmayta@gmail.com>
2024-06-10 19:45:36 -0500

    promedio

--- semana02/dia1/colecciones/Program.cs
+++ semana02/dia1/colecciones/Program.cs
@@ -4,9 +4,25 @@
 //array
 int[] notas;
 int cantidad = 0;
+
+//FUNCIONES
+double CalcularPromedioNotas(int[] notas)
+{
+    int cantidad = notas.Length;
+    int sumaNotas = 0;
+    double promedio = 0;
+
+    for(int i = 0; i < cantidad; i++)
+    {
+        sumaNotas = sumaNotas + notas[i]; //acumulador
+    }
+    promedio = sumaNotas / cantidad;
+    return promedio;
+}
+
+
 Console.WriteLine("INGRESE CANTIDAD DE NOTAS : ");
 cantidad = Convert.ToInt32(Console.ReadLine());
-
 notas = new int[cantidad];
 for (int i = 0; i < cantidad; i++)
 {
@@ -14,3 +30,5 @@ for (int i = 0; i < cantidad; i++)
     notas[i] = Convert.ToInt32(Console.ReadLine());
 }
 Console.WriteLine("se ingresaron todas las notas...");
+double promedio = CalcularPromedioNotas(notas);
+Console.WriteLine($"EL PROMEDIO DE LAS NOTAS ES : {promedio}");
