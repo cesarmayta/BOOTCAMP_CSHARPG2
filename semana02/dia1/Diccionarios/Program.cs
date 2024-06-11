d0c1e994ce216cd59e55167ab03145c0517b8b8b
César Mayta <cesarmayta@gmail.com>
2024-06-10 20:55:36 -0500

    diccionarios

---
+++ semana02/dia1/Diccionarios/Program.cs
@@ -0,0 +1,20 @@
+﻿//Diccionario
+Dictionary<string, string> capitales = new Dictionary<string, string>
+{
+    {"Perú","Lima" },
+    {"Ecuador", "Quito" },
+    {"Chile","Santiago"},
+    {"Colombia","Bogota"}
+};
+
+Console.WriteLine("Ingrese un país : ");
+string pais = Console.ReadLine();
+if (capitales.ContainsKey(pais))
+{
+    Console.WriteLine($"La capital de {pais} es {capitales[pais]}");
+}
+else
+{
+    Console.WriteLine($"No se encontró capital de {pais}");
+}
+
