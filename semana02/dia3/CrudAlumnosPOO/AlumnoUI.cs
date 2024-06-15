9a52efb8b11a8a36e912d8c6db50338781198bf3
César Mayta <cesarmayta@gmail.com>
2024-06-14 21:56:33 -0500

    crud ui listo

--- semana02/dia3/CrudAlumnosPOO/AlumnoUI.cs
+++ semana02/dia3/CrudAlumnosPOO/AlumnoUI.cs
@@ -22,10 +22,19 @@ namespace CrudAlumnosPOO
             Console.WriteLine(new string(' ', 15) + titulo);
             Console.WriteLine(new string('=', ANCHO));
         }
+
+        private void mostrarMensaje(string mensaje)
+        {
+            Console.Clear();
+            Console.WriteLine(new string('*', ANCHO));
+            Console.WriteLine(new string(' ', 15) + mensaje);
+            Console.WriteLine(new string('*', ANCHO));
+            System.Threading.Thread.Sleep(1000);
+        }
         public void MostrarMenuPrincipal()
         {
             int opcion = 0;
-            while(opcion != 5)
+            while (opcion != 5)
             {
                 Console.Clear();
                 this.mostrarTitulo("CRUD DE ALUMNOS");
@@ -39,8 +48,33 @@ namespace CrudAlumnosPOO
                 this.mostrarTitulo("INGRESE UNA OPCIÓN DEL MENU: ");
                 opcion = int.Parse(Console.ReadLine());
                 Console.Clear();
-                Console.WriteLine($"ELEGISTE OPCIÓN {opcion}");
-                System.Threading.Thread.Sleep(1000);
+
+                switch (opcion)
+                {
+                    case 1:
+                        crud.RegistrarAlumno();
+                        mostrarMensaje("ALUMNO REGISTRADO CON EXITO!!!");
+                        break;
+                    case 2:
+                        crud.MostrarAlumnos();
+                        Console.WriteLine("PRESIONE ENTER PARA CONTINUAR...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        crud.ActualizarAlumno();
+                        mostrarMensaje("ALUMNO ACTUALIZADO CON EXITO !!!");
+                        break;
+                    case 4:
+                        crud.EliminarAlumno();
+                        mostrarMensaje("ALUMNO ELIMINADO CON EXITO !!!");
+                        break;
+                    case 5:
+                        Console.WriteLine("SALIENDO DEL SISTEMA ... ");
+                        break;
+                    default:
+                        mostrarMensaje("OPCIÓN INVALIDA!!!");
+                        break;
+                }
             }
         }
     }
