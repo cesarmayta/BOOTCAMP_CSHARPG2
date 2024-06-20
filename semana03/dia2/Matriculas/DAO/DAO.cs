3aa958d6a38bbf25d9b9afb8e1f4fb4ef17de028
cesarmayta <cesarmayta@gmail.com>
2024-06-19 21:52:02 -0500

    crud profesor

--- semana03/dia2/Matriculas/DAO/DAO.cs
+++ semana03/dia2/Matriculas/DAO/DAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Matriculas.MODELS;
+using Matriculas.UTILS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace Matriculas.DAO
 {
-    internal class DAO
+    internal abstract class DAO
     {
+        protected Mensaje mensaje = new Mensaje(50);
+
+        public abstract void Create();
+        public abstract void Read();
+        public abstract void Update();
+        public abstract void Delete();
     }
 }
--- semana03/dia2/Matriculas/DAO/ProfesorDAO.cs
+++ semana03/dia2/Matriculas/DAO/ProfesorDAO.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Matriculas.MODELS;
+using Matriculas.UTILS;
 
 namespace Matriculas.DAO
 {
-    internal class ProfesorDAO
+    internal class ProfesorDAO : DAO
     {
+        private List<Profesor> listaProfesor = new List<Profesor>();
+
+        public override void Create()
+        {
+            base.mensaje.mostrarTitulo("REGISTRO DE NUEVO PROFESOR");
+            
+            Console.WriteLine("ID : ");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("NOMBRE : ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("EMAIL : ");
+            string email = Console.ReadLine();
+            Console.WriteLine("ESPECIALIDAD : ");
+            string especialidad = Console.ReadLine();
+
+            Profesor nuevoProfesor = new Profesor(id, nombre, email, especialidad);
+            listaProfesor.Add(nuevoProfesor);
+            base.mensaje.mostrarMensaje("PROFESOR REGISTRADO CON EXITO!!");
+
+        }
+        public override void Read()
+        {
+
+        }
+        public override void Update()
+        {
+
+        }
+        public override void Delete()
+        {
+
+        }
+
     }
 }
