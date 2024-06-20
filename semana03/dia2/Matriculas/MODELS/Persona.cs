e3ec03562c61908bf69c81ae44df50ca2691dec0
cesarmayta <cesarmayta@gmail.com>
2024-06-19 21:32:14 -0500

    matriculas

---
+++ semana03/dia2/Matriculas/MODELS/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriculas.MODELS
+{
+    internal class Persona
+    {
+        private int id;
+        private string nombre;
+        private string email;
+
+        public Persona(int id, string nombre, string email)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.email = email;
+        }
+
+        public virtual void Mostrar()
+        {
+            Console.WriteLine($"ID : {this.id}");
+            Console.WriteLine($"NOMBRE : {this.nombre}");
+            Console.WriteLine($"EMAIL : {this.email}");
+        }
+    }
+}
---
+++ semana03/dia2/Matriculas/MODELS/Profesor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriculas.MODELS
+{
+    internal class Profesor : Persona
+    {
+        private string especialidad;
+
+        public Profesor(int id,string nombre,string email,string especialidad) : base(id, nombre, email)
+        {
+            this.especialidad = especialidad;
+        }
+
+        public override void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine($"ESPECIALIDAD : {this.especialidad}");
+        }
+    }
+}
