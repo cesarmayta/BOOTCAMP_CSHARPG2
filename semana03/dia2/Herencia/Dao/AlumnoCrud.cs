6bd3b33b59ad892925e631062ef66cb7e8b31659
cesarmayta <cesarmayta@gmail.com>
2024-06-19 21:10:09 -0500

    abstract

---
+++ semana03/dia2/Herencia/Dao/AlumnoCrud.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia.Dao
+{
+    internal class AlumnoCrud : Crud
+    {
+        public override void Create()
+        {
+
+        }
+        public override void Read()
+        {
+
+        }
+        public override void Update()
+        {
+
+        }
+        public override void Delete()
+        {
+
+        }
+    }
+}
---
+++ semana03/dia2/Herencia/Dao/Crud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia.Dao
+{
+    internal abstract class Crud
+    {
+        public abstract void Create();
+        public abstract void Read();
+        public abstract void Update();
+        public abstract void Delete();
+    }
+}
