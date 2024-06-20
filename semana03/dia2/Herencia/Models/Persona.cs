7cb79602d0ee4e16aa8754ca7269ddcdc57bec4b
cesarmayta <cesarmayta@gmail.com>
2024-06-19 19:36:02 -0500

    dia2

---
+++ semana03/dia2/Herencia/Models/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia.Models
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+        public string Email {  get; set; }
+
+        public Persona()
+        {
+
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine(new string('=', 50));
+            Console.WriteLine($"NOMBRE : {Nombre}");
+            Console.WriteLine($"EMAIL : {Email}");
+        }
+    }
+}
---
+++ semana03/dia2/Herencia/Program.cs
@@ -0,0 +1,17 @@
+﻿using Herencia.Models;
+
+class Program
+{
+    static void Main()
+    {
+        Alumno alumno1 = new Alumno();
+        alumno1.Nombre = "César";
+        alumno1.Email = "cesar@gmail.com";
+        alumno1.Mostrar();
+
+        Profesor profesor1 = new Profesor();
+        profesor1.Nombre = "Laura";
+        profesor1.Email = "laura@gmail.com";
+        profesor1.Mostrar();
+    }
+}
