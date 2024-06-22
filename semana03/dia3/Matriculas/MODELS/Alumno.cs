28fbae1609d00ad61b87aa1077688261a7892b7c
cesarmayta <cesarmayta@gmail.com>
2024-06-21 21:31:55 -0500

    alumno

---
+++ semana03/dia3/Matriculas/MODELS/Alumno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriculas.MODELS
+{
+    internal class Alumno : Persona
+    {
+        private int nota;
+
+        public int Nota { get => nota; set => nota = value; }
+
+        public Alumno(int id, string nombre, string email, int nota) : base(id, nombre, email)
+        {
+            this.nota = nota;
+        }
+
+        public override void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine($"NOTA : {this.nota}");
+        }
+    }
+}
