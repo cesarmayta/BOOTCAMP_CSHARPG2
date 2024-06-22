2400234d5e450e7109e5fae8ed0acb1b708c5c56
cesarmayta <cesarmayta@gmail.com>
2024-06-21 21:39:50 -0500

    dao

---
+++ semana03/dia3/Matriculas/DAO/AlumnoDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Matriculas.MODELS;
+
+namespace Matriculas.DAO
+{
+    internal class AlumnoDAO : DAO
+    {
+        private List<Alumno> listaAlumnos = new List<Alumno>();
+
+        public override void Create()
+        {
+            
+        }
+
+        public override void Read()
+        {
+
+        }
+
+        public override void Update() 
+        { 
+        }
+
+        public override void Delete() 
+        { 
+        }
+
+
+    }
+}
---
+++ semana03/dia3/Matriculas/DAO/DAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriculas.DAO
+{
+    internal abstract class DAO
+    {
+        public abstract void Create();
+        public abstract void Read();
+        public abstract void Update();
+        public abstract void Delete();
+       
+    }
+}
