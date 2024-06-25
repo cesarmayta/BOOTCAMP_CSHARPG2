413470f2d0b1af2a3a0bf28d340cc95d68003c70
cesarmayta <cesarmayta@gmail.com>
2024-06-24 20:05:15 -0500

    interfaces

---
+++ semana04/dia1/Interfaces/Coche.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Coche : IVehiculo
+    {
+        private int velocidad;
+
+        public Coche()
+        {
+            this.velocidad = 0;
+        }
+
+        public void Arrancar()
+        {
+            Console.WriteLine("El coche ha arrancado.");
+        }
+
+        public void Detener()
+        {
+            Console.WriteLine("El coche se ha detenido");
+        }
+
+        public void Acelerar(int incremento)
+        {
+            this.velocidad += incremento;
+            Console.WriteLine($"El coche ha acelerado a {velocidad} km/h");
+        }
+
+        public void Frenar(int decremento)
+        {
+            velocidad -= decremento;
+            Console.WriteLine($"El coche ha frenado a {velocidad} km/h");
+        }
+    }
+}
---
+++ semana04/dia1/Interfaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using Interfaces;
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Coche miCoche = new Coche();
+
+        miCoche.Arrancar();
+        miCoche.Acelerar(50);
+        miCoche.Frenar(20);
+        miCoche.Detener();
+    }
+}
+
